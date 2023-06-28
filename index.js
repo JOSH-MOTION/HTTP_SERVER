e3b35ffcdff60bae98b1ee6dd034830fbeca88b4
JOSH <joshuadoe168@gmail>
2023-06-28 10:27:18 +0000

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const handleAllTypesOfRequests = (req,res) => {
     res.send("Respond from server.use")
 }
 
-server.use('/profile', handleAllTypesOfRequests);
-server.get('/login', (req, res) =>  res.send('hello this is the login page'))
+const handleProfilePutRequestType = (req, res) => {
+    res.send('Responded to profile request with method of value put')
+}
+server.post('/profile', handleAllTypesOfRequests);
+server.get('/login', (req, res) =>  res.send('hello this is the login page'));
+server.put('/profile', handleProfilePutRequestType);
+server.patch('/signup', (req, res) => res.send('Welcome to the signup page'))
 
 server.listen(3000, "127.0.0.1", ()=>  console.log("server is ready to accept request"));
